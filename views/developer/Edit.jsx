63e8fe6a718c615884e49f84e9ba0a3d5b71b9d2
Lucas <lucas2carlos@gmail.com>
2022-03-20 23:03:20 -0400

    Delete and Edit button for developer account

--- views/developer/Edit.jsx
+++ views/developer/Edit.jsx
@@ -7,32 +7,33 @@ class Edit extends React.Component {
         return(
             <Default account="developer" username={username}>
                 <h2>Edit {this.props.game.name} </h2>
-                <form className='needs-validation' action={`/dev/${this.props.game._id}?_method=PUT`} method="POST">
+                <form className='edit-form' action={`/dev/${this.props.game._id}?_method=PUT`} method="POST">
                     <div className='row'>
                         <div className='form-group col'>
-                            <label htmlFor='name' className='text-primary' >Name</label>
-                            <input name="name" type="text" defaultValue={this.props.game.name} className='form-control form-control-sm' id='name' required/>
+                            <label htmlFor='name' className='text-primary'>Name</label>
+                            <input name="name" type="text" defaultValue={this.props.game.name} className='form-control form-control-sm' id='name'/>
                         </div>
                         <div className='form-group col'>
                             <label htmlFor="price" className='text-primary'>Price</label>
-                            <input name="price" type="number" defaultValue={this.props.game.price} className='form-control form-control-sm' maxLength={'15'} id='price' required/>
-                            <div id='price' className="valid-feedback">Looks good!</div>
-                            <div id='price' className="invalid-feedback">Please provide price</div>
+                            <input name="price" type="number" defaultValue={this.props.game.price} className='form-control form-control-sm' id='price'/>
+                        </div>
+                        <div className='form-group col'>
+                            <label htmlFor="qty" className='text-primary'>Quantity</label>
+                            <input name="qty" type="number" defaultValue={this.props.game.qty} className='form-control form-control-sm' id='qty'/>
                         </div>
                         <div className='form-group col'>
                             <label htmlFor="img" className='text-primary'>Image</label>
-                            <input name="img" type="url" defaultValue={this.props.game.img} className='form-control form-control-sm' id='url' required/>
-                            <div id='url' className="valid-feedback">Looks good!</div>
-                            <div id='url' className="invalid-feedback">Please provide url</div>
+                            <input name="img" type="url" defaultValue={this.props.game.img} className='form-control form-control-sm' id='url'/>
                         </div>
                     </div>
                     <div className='form-group'>
                         <label htmlFor="description" className='text-primary'>  Description</label>
-                        <textarea name="description" defaultValue={this.props.game.description} id="description description-box" className='form-control' cols="40" rows="3" required></textarea>
-                        <div id='description' className="valid-feedback">Looks good!</div>
-                        <div id='description' className="invalid-feedback">Please provide description</div>
+                        <textarea name="description" defaultValue={this.props.game.description} id="description description-box" maxLength={'500'} className='form-control' cols="40" rows="3"></textarea>
                     </div>
                     <input className='btn btn-outline-success' type="submit" value="Edit Game" />
+                </form>
+                <form action={`/dev/${this.props.game._id}?_method=DELETE`} method="POST">
+                    <input className='btn btn-danger' type="submit" value='Delete Game' />
                 </form>
             </Default>
         )
--- views/developer/Show.jsx
+++ views/developer/Show.jsx
@@ -5,36 +5,39 @@ class Show extends React.Component {
     render(){
         const {game, username} = this.props
         const reviews = game.reviews
+        // const background = {background: 'linear-gradient(rgba(0, 123, 255, 0.37) ,rgba(0, 0, 0) 85%)'}
         return(
-            <Default account="gamer" username={username}>
-                <div className='show-div'>
-                    <h2>{game.name}</h2>
-                    <img src={game.img} alt={game.name} />
-                    <div className='about-div form-group'>
-                        <h3>About this Game:</h3>
-                        <hr />
-                        <div className='about-description rounded'>
-                            <p className=''>{game.description}</p>
-                        </div>
-                    </div>
-                    <div className='review-div form-group'>
-                        <h3>Customer Reviews</h3>
-                        <hr />
-                        <div className="review-comments">
-                            {
-                                reviews.map((review) => {
-                                    return(
-                                        <div className='form-group'>
-                                        <small>{review.name}</small>
-                                        <p key={game._id} className="">{review.description}</p>
-                                    </div>
-                                    )
-                                })
-                            }
-                        </div>
+        <Default account="developer" username={username}>
+            <div className='show-div'>
+                <a style={{padding: '5px !Important', fontSize: '14px !Important'}} className="nav-link btn btn-info" href={`/dev/${game._id}/edit`}>Edit</a>
+                <h2>{game.name}</h2>
+                <img src={game.img} alt={game.name} />
+                <p style={{background: 'linear-gradient(rgba(0, 123, 255, 0.37) ,rgba(0, 0, 0) 85%)', marginBottom: '0', padding: '2px'}}>Quantity: {game.qty}</p>
+                <div className='about-div form-group'>
+                    <h3>About this Game:</h3>
+                    <hr />
+                    <div className='about-description rounded'>
+                        <p className=''>{game.description}</p>
                     </div>
                 </div>
-            </Default>
+                <div className='review-div form-group'>
+                    <h3>Customer Reviews</h3>
+                    <hr />
+                    <div className="review-comments">
+                        {
+                            reviews.map((review) => {
+                                return(
+                                    <div className='form-group'>
+                                    <small>{review.name}</small>
+                                    <p key={game._id} className="">{review.description}</p>
+                                </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </Default>
         )
 
     }
